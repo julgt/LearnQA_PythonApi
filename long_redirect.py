f052c4238f5a3648e555c6a32c2b03e6710c226a
julgt <julgt@mail.ru>
2022-06-26 00:22:45 +0400

    long_redirect

---
+++ long_redirect.py
@@ -0,0 +1,10 @@
+import requests
+
+response = requests.post('https://playground.learnqa.ru/api/long_redirect', allow_redirects=True)
+count_url = len(response.history)
+print(f"Число редиректов: {count_url-1}")
+
+for i in range(count_url):
+    print(response.history[i].url)
+    if i == count_url-1:
+        print(f"Итоговый URL: {response.history[i].url}")

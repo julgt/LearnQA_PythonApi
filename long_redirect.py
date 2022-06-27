a344b0fc9cfed409ec0bf3e9a662b86d77505cb7
julgt <julgt@mail.ru>
2022-06-27 19:59:06 +0400

    правки long_redirect.py

--- long_redirect.py
+++ long_redirect.py
@@ -2,7 +2,7 @@ import requests
 
 response = requests.post('https://playground.learnqa.ru/api/long_redirect', allow_redirects=True)
 count_url = len(response.history)
-print(f"Число редиректов: {count_url-1}")
+print(f"Число редиректов: {count_url}")
 
 for i in range(count_url):
     print(response.history[i].url)
